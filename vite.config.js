4f81df2b9047f62cc7089f71ecc82e1819fa1ec9
FMora <fidel.mora.aguirre@gmail.com>
2025-06-01 17:51:34 -0400

    Fix reload previsualizacion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/04React_01Pizzeria-Mamma-Mia/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notFound: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 })
